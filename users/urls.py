88eb4c7580eabd17b471ef7655ac4658eaf08090
Trevor5F <101113379+Trevor5F@users.noreply.github.com>
2023-12-12 21:59:03 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -23,13 +23,3 @@ urlpatterns = [
     path('user/<int:pk>/upload_image/', UserUploadImageView.as_view(), name='user_upload_image'),
 ]
 
-'''
-GET "users/" — список профилей пользователей
-POST "users/" — регистрация пользователя
-GET, PATCH, DELETE "users/{id}" — в соотвествии с REST и необходимыми permissions (для администратора)
-GET PATCH "users/me" — получение и изменение своего профиля
-POST "users/set_password" — ручка для изменения пароля
-
-POST "users/reset_password" — ручка для направления ссылки сброса пароля на email*
-POST "users/reset_password_confirm" — ручка для сброса своего пароля*
-'''
